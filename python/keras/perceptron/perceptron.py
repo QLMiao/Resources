8bda7413783234a33ae2c498951a9eca800afec8
QLMiao <411825877@qq.com>
2018-06-12 04:31:00 -0700

    added mnist.

--- python/keras/perceptron/perceptron.py
+++ python/keras/perceptron/perceptron.py
@@ -1,25 +1,38 @@
-#!/usr/bin/python3
+#!/home/mql/anaconda3/bin/python3
 
-# Create first network with Keras
+# Create first network with Kerase
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.cross_validation import cross_val_score
 import numpy
+import pandas
+
+# Funtion to create model, required for KerasClassifier
+def create_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(12, input_dim=8, init='uniform', activation='relu')) 
+	model.add(Dense(8, init='uniform', activation='relu'))
+	model.add(Dense(1, init='uniform', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) 
+	return model
+	
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
+
 # load pima indians dataset
 dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:8]
 Y = dataset[:,8]
-# create model
-model = Sequential()
-model.add(Dense(12, input_dim=8, init='uniform', activation='relu')) 
-model.add(Dense(8, init='uniform', activation='relu'))
-model.add(Dense(1, init='uniform', activation='sigmoid'))
-# Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) # Fit the model
-model.fit(X, Y, validation_split=0.33,nb_epoch=150, batch_size=10)
-# evaluate the model
-scores = model.evaluate(X, Y)
-print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+# create model 
+model=KerasClassifier(build_fn=create_model,nb_epoch=150, batch_size=10)
+
+# evaluate using 10-fold corss validation
+kfold=StratifiedKFold(y=Y,n_folds=10,shuffle=True,random_state=seed)
+results=cross_val_score(model,X,Y,cv=kfold)
+print(results.mean())
